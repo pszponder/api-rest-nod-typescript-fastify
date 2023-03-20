ce632e2be1e123e6bd02d49e85333646be28e5bf
Piotr Szponder <14116413+pszponder@users.noreply.github.com>
2023-03-20 13:28:23 -0600

    feat: enable HTTPS and HTTP/2 on fastify server

--- src/api/buildServer.ts
+++ src/api/buildServer.ts
@@ -1,8 +1,15 @@
 import Swagger from "@fastify/swagger";
 import SwaggerUI from "@fastify/swagger-ui";
 import fastify from "fastify";
+import { readFile } from "node:fs/promises";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import { envVars as env } from "../utils/parseEnvVars.js";
 import { itemRoutes } from "./resources/items/items.routes.js";
+
+// Get the directory path of this file
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 /**
  * Create a configuration object to configure the logger based on the NODE_ENV
@@ -28,9 +35,15 @@ const configLogger = {
  * Instantiates and returns a configured Fastify server instance
  * @returns Fastify Server Instance
  */
-export function createServer() {
+export async function createServer() {
   // Instantiate Fastify Server and configure logging
   const server = fastify({
+    http2: true,
+    https: {
+      allowHTTP1: true, // fallback support for HTTP1
+      key: await readFile(`${__dirname}/../certs/key.pem`),
+      cert: await readFile(`${__dirname}/../certs/cert.pem`),
+    },
     logger: configLogger[env.NODE_ENV as keyof typeof configLogger] ?? true,
   });
 
